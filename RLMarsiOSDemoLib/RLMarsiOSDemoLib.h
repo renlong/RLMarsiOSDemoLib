8913513601bfc8e1a792e25c8a93ea1e49e1670b
renlong <renlong@taptap.com>
2022-04-18 16:06:57 +0800

    创建Demo项目

---
+++ RLMarsiOSDemoLib/RLMarsiOSDemoLib.h
@@ -0,0 +1,18 @@
+//
+//  RLMarsiOSDemoLib.h
+//  RLMarsiOSDemoLib
+//
+//  Created by 任龙 on 2022/4/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RLMarsiOSDemoLib.
+FOUNDATION_EXPORT double RLMarsiOSDemoLibVersionNumber;
+
+//! Project version string for RLMarsiOSDemoLib.
+FOUNDATION_EXPORT const unsigned char RLMarsiOSDemoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RLMarsiOSDemoLib/PublicHeader.h>
+
+
